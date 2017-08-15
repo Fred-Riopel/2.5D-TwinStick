7874fd567edc4f68bad05c26f957cee1b3215a40
Fred Riopel <frederic.r.poissant@gmail.com>
2017-08-15 13:43:53 -0400

    Script Update + some game design

---
+++ 10-TwinStick-e15d3a2fc0f2e2a31bf3c33f1b2a886891cc0394/Assets/Camera/MyCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCamera : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        // Camera follows the player with specified offset, except if he falls
+        if(player.position.y >= -5)
+        {
+            transform.position = new Vector3(player.position.x + offset.x,
+            player.position.y + offset.y, offset.z);
+        }
+    }
+}
---
+++ 10-TwinStick-e15d3a2fc0f2e2a31bf3c33f1b2a886891cc0394/Assets/Player/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject startPointPole, checkPointPole;
+
+    private GameObject player;
+    private Vector3 startPoint, checkPoint;
+    private bool isCheckPointReached = false;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        startPoint = new Vector3(startPointPole.transform.position.x, 0, 0);
+        checkPoint = new Vector3(checkPointPole.transform.position.x, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if(player.transform.position.x >= checkPointPole.transform.position.x)
+        {
+            isCheckPointReached = true;
+        }
+
+		if(player.transform.position.y < -50)
+        {
+            PlayerFellInHole();
+        }
+	}
+
+    private void PlayerFellInHole()
+    {
+        if (isCheckPointReached)
+        {
+            player.transform.position = checkPoint;
+        }
+        else
+        {
+            player.transform.position = startPoint;
+        }
+    }
+}
